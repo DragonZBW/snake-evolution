4888b7c64386393a1e25cdd17a024d980cb175e6
DragonZBW <dragonzbw@gmail.com>
2022-04-15 11:39:04 -0400

    Began creating a Population class
    - This class will represent an individual generation within the simulation

---
+++ src/population.js
@@ -0,0 +1,14 @@
+// A class representing a population of neural networks. 
+// This is used to represent all the networks in a generation and keep track of them in relation to each other.
+class Population {
+    // Construct a population with an initial size and template neural network to base the initial condition off of.
+    constructor(size, templateNN) {
+        this.size = size;
+        this.networks = [];
+        for (let i = 0; i < size; i++) {
+            const nn = templateNN.copy();
+            nn.randomizeConnectionWeights();
+            this.networks.push(nn);
+        }
+    }
+}
